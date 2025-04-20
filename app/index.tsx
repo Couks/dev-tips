eccb542cb17ab7908cf2ed170e2be0e98e0732df
Matheus Castro <49662686+Couks@users.noreply.github.com>
2025-04-19 21:00:39 -0300

    finalizada estilização da tabbar

--- app/index.tsx
+++ app/index.tsx
@@ -4,60 +4,80 @@ import HomeScreen from "./screens/HomeScreen"
 import CategoriesScreen from "./screens/CategoriesScreen"
 import ChallengesScreen from "./screens/ChallengesScreen"
 import ProfileScreen from "./screens/ProfileScreen"
+import { View } from "react-native"
 
 const Tab = createBottomTabNavigator()
 
 export default function TabLayout() {
-  
   return (
     <Tab.Navigator
       screenOptions={{
-        tabBarActiveTintColor: "#6366f1",
-        tabBarInactiveTintColor: "#6366f8",
         tabBarShowLabel: false,
         tabBarStyle: {
-          borderTopWidth: 0,
-          paddingTop: 8,
-          paddingBottom: 8,
-          backgroundColor: "white",
+          position: "absolute",
+        
+          left: 16,
+          right: 16,
+          elevation: 8,
+          backgroundColor: "#ffffff",
+        
+          height: 60,
+          paddingBottom: 10,
+          paddingTop: 10,
           shadowColor: "#000",
-          shadowOpacity: 0.1,
-          shadowRadius: 4,
-          elevation: 2,
-  
+          shadowOffset: {
+            width: 0,
+            height: 4,
+          },
+          shadowOpacity: 0.15,
+          shadowRadius: 8,
         },
+        tabBarActiveTintColor: "#000",
+        tabBarInactiveTintColor: "#aaa",
       }}
     >
       <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{
-          tabBarIcon: ({ color }) => <Home size={22} color={color} />,
-          title: "Dica do Dia",
+          tabBarIcon: ({ color, focused }) => (
+            <View style={{ alignItems: "center", justifyContent: "center" }}>
+              <Home size={focused ? 28 : 24} color={color} />
+            </View>
+          ),
         }}
       />
       <Tab.Screen
         name="Categories"
         component={CategoriesScreen}
         options={{
-          tabBarIcon: ({ color }) => <FolderOpen size={22} color={color} />,
-          title: "Categorias",
+          tabBarIcon: ({ color, focused }) => (
+            <View style={{ alignItems: "center", justifyContent: "center" }}>
+              <FolderOpen size={focused ? 28 : 24} color={color} />
+            </View>
+          ),
         }}
       />
       <Tab.Screen
         name="Challenges"
         component={ChallengesScreen}
         options={{
-          tabBarIcon: ({ color }) => <Target size={22} color={color} />,
-          title: "Desafios",
+          tabBarIcon: ({ color, focused }) => (
+            <View style={{ alignItems: "center", justifyContent: "center" }}>
+              <Target size={focused ? 28 : 24} color={color} />
+            </View>
+          ),
         }}
       />
       <Tab.Screen
         name="Profile"
         component={ProfileScreen}
         options={{
-          tabBarIcon: ({ color }) => <User size={22} color={color} />,
-          title: "Perfil",
+          tabBarIcon: ({ color, focused }) => (
+            <View style={{ alignItems: "center", justifyContent: "center" }}>
+              <User size={focused ? 28 : 24} color={color} />
+            </View>
+          ),
         }}
       />
     </Tab.Navigator>
